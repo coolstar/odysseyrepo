4a2061fd7e1e1c5e113774468d83c659f2988334
CoolStar <coolstarorganization@gmail.com>
2020-11-20 09:00:17 -0800

    Update scripts

---
+++ updaterelease.php
@@ -0,0 +1,9 @@
+<?php
+	$release = file_get_contents("Release-template");
+	$release .= "Date: ".date("D, j M Y H:i:s O")."\n";
+	$release .= "SHA256:\n";
+	foreach (["Packages", "Packages.bz2", "Packages.xz"] as $i){
+		$release .= " ".hash_file("sha256", $i)." ".filesize($i)." ".$i."\n";
+	}
+	file_put_contents("Release", $release);
+?>
