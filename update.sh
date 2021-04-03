145be622d634d5d5e71a08621a27cf044ecea24b
CoolStar <coolstarorganization@gmail.com>
2021-04-02 22:07:33 -0700

    Update libhooker

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 dpkg-scanpackages debs > Packages.tmp
-rm -f Packages Packages.bz2 Packages.xz
+rm -f Packages Packages.bz2 Packages.xz Packages.zst
 cp Packages.tmp Packages
 bzip2 Packages
 cp Packages.tmp Packages
