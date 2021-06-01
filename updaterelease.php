17e7c1ee1179a0baff2d5d8ac8ec03d58a185c98
Hayden <me@diatr.us>
2021-06-01 17:50:10 -0400

    Add zst hash

--- updaterelease.php
+++ updaterelease.php
@@ -2,7 +2,7 @@
 	$release = file_get_contents("Release-template");
 	$release .= "Date: ".date("D, j M Y H:i:s O")."\n";
 	$release .= "SHA256:\n";
-	foreach (["Packages", "Packages.bz2", "Packages.xz"] as $i){
+	foreach (["Packages", "Packages.bz2", "Packages.xz", "Packages.zst"] as $i){
 		$release .= " ".hash_file("sha256", $i)." ".filesize($i)." ".$i."\n";
 	}
 	file_put_contents("Release", $release);
