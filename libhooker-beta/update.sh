9920b01e1595ddd0c70d5dbc8806ceb235fd209b
Hayden <me@diatr.us>
2021-07-20 15:05:32 -0400

    Update LH beta repo

--- libhooker-beta/update.sh
+++ libhooker-beta/update.sh
@@ -1,8 +1,11 @@
 dpkg-scanpackages debs > Packages.tmp
-rm -f Packages Packages.bz2 Packages.xz
+rm -f Packages Packages.bz2 Packages.xz Packages.zst
 cp Packages.tmp Packages
 bzip2 Packages
 cp Packages.tmp Packages
 xz Packages
+cp Packages.tmp Packages
+zstd Packages
 mv Packages.tmp Packages
 php updaterelease.php
+gpg -abs -u 2703D09477F7EFC9B96821905710F00A25078B2D -o Release.gpg Release
